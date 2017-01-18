3c844aac8932bb94bc0d502c49a787a73c763e6f
Denis Gathondu <thundoss@gmail.com>
2017-01-18 14:21:29 +0300

    Added try/except clause to capture locations not found

--- weather.py
+++ weather.py
@@ -10,19 +10,27 @@ BASE_URL = "http://api.wunderground.com/api/{}/conditions/q".format(secrets.KEY)
 def main():
 
     # prompt user for input
-    location = input("Where would you like to get the weather for? ")
+    location = input("Where would you like to get the weather for?"
+                     "[city, state] ").capitalize()
 
     # request for the weather update of location user entered
     response = requests.get(BASE_URL + "/{}.json".format(location))
     weather = response.json()
-    current = weather['current_observation']
-    country = current['display_location']['country']
-    city = current['display_location']['city']
-    state = current['display_location']['state']
-    temp = current['temperature_string']
-    condition = current['weather']
 
-    print(weather)
+    # check if location was found and notify if it wasn't found
+    try:
+        current = weather['current_observation']
+        country = current['display_location']['country']
+        city = current['display_location']['city']
+        state = current['display_location']['state']
+        temp = current['temperature_string']
+        condition = current['weather']
+    except KeyError:  # if a location is not found the reponse does not have ['current_observation'] key
+        print("Could not find that location")
+        # run again
+        main()
+
+    print("Currently its {}, {} in {} {}, {}".format(condition, temp, city, state, country))
 
 
 if __name__ == "__main__":
